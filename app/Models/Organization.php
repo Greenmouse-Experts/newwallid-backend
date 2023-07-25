ddd42e7f7e7e38eed4457c7fb6fcb4f942830b72
cleanprogrammer2026 <cleanprogrammer2026@gmail.com>
2023-07-25 14:16:46 +0100

    members size display

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Member;
 
 class Organization extends Model
 {
@@ -14,5 +15,12 @@ class Organization extends Model
     public function user()
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public static function member_size($org_id) {
+        $members = Member::where(['organization_id' => $org_id])
+            ->get();
+
+        return sizeof($members);
     }
 }
